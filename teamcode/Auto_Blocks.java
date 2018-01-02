21bcf88035ac05d263f2e9d1e54a0eee0cdb9cff
CH-Prog <christian.holtzen@gmail.com>
2018-01-02 12:15:18 -0600

    MyStuff

---
+++ teamcode/Auto_Blocks.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+/**
+ * Created by LCL on 12/29/2017.
+ */
+
+@Disabled
+public class Auto_Blocks {
+
+    public void Drive(){
+
+    }
+
+    public void Turn(){
+
+    }
+
+    public void Reverse(){
+
+    }
+
+    public void Lift_Arm(){
+
+    }
+
+    public void Lower_Arm(){
+
+    }
+
+    public void Release_Claws(){
+
+    }
+
+
+
+
+
+}
